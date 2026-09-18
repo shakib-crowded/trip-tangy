41a3bbd928045eafa37cec63c979e5fab647034f
Shakib Ansari <shakibansari7406@gmail.com>
2026-09-18 10:23:33 +0530

    Change HotelList component UI.

--- app/components/Hotels/HotelList.tsx
+++ app/components/Hotels/HotelList.tsx
@@ -39,7 +39,7 @@ export default function HotelList() {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {hotels.map((hotel) => (
             <HotelCard key={hotel._id} hotel={hotel} />
           ))}
