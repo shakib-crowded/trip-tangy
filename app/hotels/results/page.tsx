364b67704108ed8386fe363dca3948ca034ccab4
Shakib Ansari <shakibansari7406@gmail.com>
2026-09-18 10:29:10 +0530

    Change in Search Results

--- app/hotels/results/page.tsx
+++ app/hotels/results/page.tsx
@@ -62,7 +62,7 @@ export default async function HotelResultsPage({
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {sorted.map((hotel) => (
             <HotelCard key={hotel._id} hotel={hotel} />
           ))}
